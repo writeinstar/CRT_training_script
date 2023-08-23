cde5901b8f7ddb79b40fa57ae74359bc4041de74
Patrick Kievit <pkievit@copado.com>
2023-08-23 12:45:45 +0000

    test

--- exercise-7-using-variables/DynamicVariables.py
+++ exercise-7-using-variables/DynamicVariables.py
@@ -19,17 +19,17 @@ def get_area(diameter):
 AREA1 = get_area(1)
 AREA2 = get_area(2)
 
-dev = {'scalar': 'Scalar variable',
-              'LIST__list': ['List','variable']}
-uat = {'scalar' : 'Some other value',
-              'LIST__list': ['Some','other','value'],
-              'extra': 'variables1 does not have this at all'}
+# dev = {'scalar': 'Scalar variable',
+#               'LIST__list': ['List','variable']}
+# uat = {'scalar' : 'Some other value',
+#               'LIST__list': ['Some','other','value'],
+#               'extra': 'variables1 does not have this at all'}
 
-def get_variables(env):
-    if env == 'dev':
-        return dev
-    elif env == 'uat':
-        return uat
-    else:
-        # default variables
-        return uat
+# def get_variables(env):
+#     if env == 'dev':
+#         return dev
+#     elif env == 'uat':
+#         return uat
+#     else:
+#         # default variables
+#         return uat
