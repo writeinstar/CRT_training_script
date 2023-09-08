91bfc07338f11aa9a6f4f596cffeb57c84ee93d3
Patrick Kievit <pkievit@copado.com>
2023-09-08 13:45:49 +0000

    test

--- 07__exercise-using-variables/DynamicVariables.py
+++ 07__exercise-using-variables/DynamicVariables.py
@@ -3,7 +3,12 @@ import random
 import time
 import math
 
+# USER = os.getlogin()                # current login name
+
+RANDOM_INT = random.randint(0, 10)  # random integer in range [0,10]
+
 CURRENT_TIME = time.asctime()       # timestamp like 'Thu Apr  6 12:45:21 2006'
+
 if time.localtime()[3] > 12:
     AFTERNOON = True
 else:
