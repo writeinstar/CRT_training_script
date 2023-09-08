db9a745621efd9c71c82c5f2a99068201240f295
Patrick Kievit <pkievit@copado.com>
2023-09-08 14:01:35 +0000

    test

--- 07__exercise-using-variables/DynamicVariables.py
+++ 07__exercise-using-variables/DynamicVariables.py
@@ -3,8 +3,6 @@ import random
 import time
 import math
 
-USER = os.getlogin()                # current login name
-
 RANDOM_INT = random.randint(0, 10)  # random integer in range [0,10]
 
 CURRENT_TIME = time.asctime()       # timestamp like 'Thu Apr  6 12:45:21 2006'
