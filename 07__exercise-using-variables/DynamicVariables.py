deff32e8b235dae30c0862d2403f02762e1c2245
Patrick Kievit <pkievit@copado.com>
2023-09-08 13:53:44 +0000

    test

--- 07__exercise-using-variables/DynamicVariables.py
+++ 07__exercise-using-variables/DynamicVariables.py
@@ -3,7 +3,7 @@ import random
 import time
 import math
 
-# USER = os.getlogin()                # current login name
+USER = os.getlogin()                # current login name
 
 RANDOM_INT = random.randint(0, 10)  # random integer in range [0,10]
 
